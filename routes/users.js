bef54f2768283a8f4801ffd43ff1c3ba59151ea4
Amaan Ansari <amaanansari@Amaans-MacBook-Pro.local>
2022-06-25 14:14:52 +0530

    express question 3

---
+++ routes/users.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+    router = express.Router(),
+    userController=require('../controllers/user'),
+    protectedRoute=require('../middleware/protectRoute')
+
+
+
+
+
+
+//post
+router.post('/register',userController.registerController)
+
+router.post('/login',userController.loginController)
+
+
+router.post('/address',protectedRoute,userController.addressController) //protected
+
+
+//get
+
+router.get('/list/:page',userController.listController) //protected
+
+
+
+
+
+
+
+
+
+
+    module.exports= router;
